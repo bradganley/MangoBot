5cdef9afa6b22a787f35497abde178b39a28305c
Brad Ganley <bradart.ink@gmail.com>
2020-12-14 01:14:23 -0600

    Howdy

---
+++ README.md
@@ -0,0 +1,3 @@
+# DemoBot
+
+It's just a robot that we can offload mundane tasks to. It will very likely be ugly and unimpressive. Maybe it won't, though. Then won't you feel like a jackass.
---
+++ bot.js
@@ -0,0 +1,34 @@
+require('dotenv').config();
+var auth = require('./auth.json');
+const Discord = require('discord.js');
+const querystring = require('querystring');
+//BOT_URL=https://discord.com/oauth2/authorize?client_id=787936572754493481&scope=bot&permissions=268691458
+const prefix = "🥭:";
+const client = new Discord.Client();
+
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}. I'm ready for you, Daddy`);
+});
+client.on('message', async message => {
+    try{
+    if (!message.content.startsWith(prefix) || message.author.bot) {
+        return;
+    }
+
+    const args = message.content.slice(prefix.length).trim().split(' ');
+    const command = args.shift().toLowerCase();
+
+    if (command === "list") {
+        for (i in args) {
+            message.reply(`Command: ${command}\nArguments: ${args[i]}`);
+        }
+    }
+    if (command === "hello") {
+        message.reply(`Hey.`);
+    }
+    
+}  catch(err) {message.reply(err);}
+});
+
+
+client.login(process.env.CLIENT_TOKEN);
