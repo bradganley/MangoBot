3ab46d6ded250e8b6284d7fe30207e759abf3003
Brad Ganley <bradart.ink@gmail.com>
2020-12-21 01:47:45 -0600

    multiple results in strain search

--- commands/strain.js
+++ commands/strain.js
@@ -3,37 +3,48 @@ const client = new Discord.Client();
 const fetch = require('node-fetch');
 
 const embed = new Discord.MessageEmbed()
-.setColor(0x91EE60)
-.setAuthor('Mangobot Strain Search');
+  .setColor(0x91EE60)
+  .setAuthor('Mangobot Strain Search');
 
 const strains = async (message, args) => {
   try {
     let strName = "";
     for (i in args) {
       let strBoy;
-      if(args.length > 1){
-        strBoy = strName.concat(args[i],"%20");
-      console.log(args[i]);
- 
+      if (args.length > 1) {
+        strBoy = strName.concat(args[i], "%20");
+        console.log(args[i]);
+
       } else {
         strBoy = strName.concat(args[i])
-       }
+      }
       strName = strBoy;
     }
-    try { 
-     let strainLoad = await fetch(`https://strainapi.evanbusse.com/A1E9hg5/strains/search/name/${strName.slice(0, strName.length-3)}`);
-     //message.reply(strName);
-     strSon = await strainLoad.json();
-     //message.reply(strSon[0].name);
-     embed.setTitle(strSon[0].name)
-     .attachFiles(['assets/straincube.png'])
-     .setThumbnail('attachment://straincube.png')
-     //.setField(`Name:`, `${strSon[0].name}`, false)
-     .setDescription(`${strSon[0].desc}`);
-     message.reply(embed);
-    } catch (err){ console.log(err);}
+    try {
+      let strainLoad = await fetch(`https://strainapi.evanbusse.com/A1E9hg5/strains/search/name/${strName.slice(0, strName.length - 3)}`);
+      //message.reply(strName);
+      strSon = await strainLoad.json();
+      let endBarf = "";
+      let count = 1;
+      for (i in strSon) {
+        
+        if (count < 4) {
+          let tempo
+          tempo = endBarf.concat(`**Name**: ${strSon[i].name}\n**About**: ${strSon[i].desc}\n\n`);
+          console.log('fuck');
+          endBarf = tempo;
+          count++;
+        }
+      }
+      embed.setTitle('Search Results')
+        .attachFiles(['assets/straincube.png'])
+        .setThumbnail('attachment://straincube.png')
+        //.setField(`Name:`, `${strSon[0].name}`, false)
+        .setDescription(endBarf);
+      message.reply(embed);
+    } catch (err) { console.log(err); }
 
-    
+
   } catch (err) { console.log(err) }
 }
 
