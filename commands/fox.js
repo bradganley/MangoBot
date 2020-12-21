dda952fde2dcfe842574b7e7005f7b5a16d2895d
Brad Ganley <bradart.ink@gmail.com>
2020-12-21 13:35:43 -0600

    fixed fox

--- commands/fox.js
+++ commands/fox.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 const fetch = require('node-fetch');
 
-const fox = async () => {
+const fox = async (message, args) => {
     const foxPic = await fetch('https://randomfox.ca/floof/');
     foxSon = await foxPic.json();
     message.reply(foxSon.image);
@@ -11,6 +11,6 @@ module.exports = {
     name: 'fox',
 	description: 'Shows you a cute fox',
     execute(message, args) {
-        fox();
+        fox(message,args);
 	}
 }
