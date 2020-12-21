4a15a4d1d5681c73e833527b80fcd0c626160e8b
Brad Ganley <bradart.ink@gmail.com>
2020-12-21 15:53:54 -0600

    fixed it(?)

--- commands/meow.js
+++ commands/meow.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 const fetch = require('node-fetch');
 
-const meow = async () => {
+const meow = async (message, args) => {
     const catPic = await fetch('https://aws.random.cat/meow');
     catSon = await catPic.json();
     message.reply(catSon.file);
