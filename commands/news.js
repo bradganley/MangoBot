6b76a21a7864494699ec75cf4ad06d40818aa5a3
Brad Ganley <bradart.ink@gmail.com>
2020-12-22 01:56:07 -0600

    implemented newsreader

---
+++ commands/news.js
@@ -0,0 +1,34 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const fetch = require('node-fetch');
+const Parser= require('rss-parser');
+let parser= new Parser();
+
+const embed = new Discord.MessageEmbed()
+  .setColor(0x91EE60)
+  .setAuthor('Cannabis News');
+
+const feedGrab = async (message, args) => {
+  let feed = await parser.parseURL('https://www.reddit.com/r/cannabis.rss');
+  console.log(feed.title);
+  message.reply(feed.title);
+  let embedBoy = "";
+  for(i = 0; i < 3; i++){
+    console.log(`**${feed.items[i].title}` + ':\n\t' + `**${feed.items[i].link}**` + '\n')
+    embedBoy +=  `**${feed.items[i].title}**` + ':\n \t' + `${feed.items[i].link}` + '\n\n';
+  }
+  embed.setDescription(embedBoy)
+  .attachFiles(['assets/newsLogo.png'])
+  .setThumbnail('attachment://newsLogo.png')
+  .setFooter('coded by Brad Ganley, ruined by society');
+  message.reply(embed);
+}
+
+
+module.exports = {
+  name: 'news',
+  description: 'Gets strain info from the strain API',
+  execute(message, args) {
+    feedGrab(message, args);
+  }
+}
