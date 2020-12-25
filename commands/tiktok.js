aafcfc8037731aecb5483b3ff3fdf9283e82c7ea
Brad Ganley <bradart.ink@gmail.com>
2020-12-25 15:34:54 -0600

    created tiktok embed

--- commands/tiktok.js
+++ commands/tiktok.js
@@ -1,4 +1,4 @@
-/* const Discord = require('discord.js');
+const Discord = require('discord.js');
 const client = new Discord.Client();
 const fetch = require('node-fetch');
 
@@ -10,7 +10,7 @@ const header = new fetch.Headers({
 const getUser = async (message, args) => {
     let query = endPoint + args[0];
     try {
-        let payLoad = await fetch(`https://tiktok4.p.rapidapi.com/user/profile?user_name=${args[0]}`, {
+        let payLoad = await fetch(query, {
             "method": "GET",
             "headers": {
                 "x-rapidapi-key": process.env.RAPID_KEY,
@@ -18,7 +18,15 @@ const getUser = async (message, args) => {
             }
         });
         let paySon = await payLoad.json();
-        message.reply(paySon.userInfo.user.bioLink.link);
+        let embed = new Discord.MessageEmbed()
+        .setColor('#EE1D52')
+        .setTitle(paySon.userInfo.user.nickname)
+        .setThumbnail(paySon.userInfo.user.avatarMedium)
+        .setDescription(`_**${paySon.metaParams.title}**_\n
+        ${paySon.metaParams.description}\n${paySon.metaParams.canonicalHref}`)
+        .setURL(paySon.metaParams.cononicalHref)
+        .setFooter(`Followers: ${paySon.userInfo.stats.followerCount}, Videos: ${paySon.userInfo.stats.videoCount}`);
+        message.channel.send(embed);
         
     } catch (err) {
         console.log(err);
@@ -32,4 +40,3 @@ module.exports = {
         getUser(message, args);
     }
 }
-*/
