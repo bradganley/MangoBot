c1806b8017f3b7e86c92984c56067423388e2aff
Brad Ganley <bradart.ink@gmail.com>
2020-12-28 16:10:37 -0600

    ok

--- commands/hello.js
+++ commands/hello.js
@@ -1,20 +1,6 @@
-function sleep(ms) {
-    return new Promise((resolve) => {
-      setTimeout(resolve, ms);
-    });
-  }
-  
-  function between(min, max) {
-    return Math.floor(
-      Math.random() * (max - min) + min
-    )
-  }
-  const hello = async () => {
-    await sleep(3000);
+const hello = async () => {
         message.reply(`Hey.`);
         console.log("got it");
-        await sleep(30000);
-        message.reply('lololol made you look');
 }
 module.exports = {
     name: 'hello',
