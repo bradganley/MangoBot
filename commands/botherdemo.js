79d3a6164be95b37576498e45c54b2acff9f7e7e
Brad Ganley <bradart.ink@gmail.com>
2021-01-01 18:51:05 -0600

    Fixing botherdemo I guess?

--- commands/botherdemo.js
+++ commands/botherdemo.js
@@ -9,7 +9,7 @@ function sleep(ms) {
       Math.random() * (max - min) + min
     )
   }
-let bother = async () => {
+let bother = async (message, args) => {
     console.log("Ok, gonna annoy demo");
     await sleep(between(5000, 180000));
     message.channel.send("Hi, <@667084378672726022> lol");
@@ -18,6 +18,6 @@ module.exports = {
     name: 'botherdemo',
     description: 'Bothers Demo',
     execute(message, args) {
-        bother();
+        bother(message, args);
     }
 }
